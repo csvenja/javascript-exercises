045f7a32e5d481cb144362315401585e53f88508
csvenja <c.svenjax@gmail.com>
2014-07-29 10:48:00 -0800

    querySelectorALL

---
+++ Query Selector All.js
@@ -0,0 +1,6 @@
+0. document.getElementsByTagName("*");
+1. document.querySelectorAll("p.big");
+2. document.getElementById("main").getElementsByTagName("p")[0];
+   document.querySelectorAll("#main p:first-child");
+3. document.querySelectorAll("tr:nth-child(odd)");
+4. document.querySelectorAll("input[type=textarea]");
